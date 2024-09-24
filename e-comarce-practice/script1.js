6995cd68609cec9e0a55ae95490bab4586253ed9
shubhambudde1 <shubhambudde.github@gmail.com>
2024-09-23 17:22:29 -0700

    all file
    add file regarding  ecommerse

--- e-comarce-practice/script1.js
+++ e-comarce-practice/script1.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index1.html");
+  res.sendFile(__dirname + "/index.html");
 });
+
 app.listen(3000, function () {
-  console.log("Server is running on localhost3000");
+  console.log("Server is running on localhost:3000");
 });
